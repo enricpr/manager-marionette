f99b39083fc1230108daae1b0fa4b23f47833dc6
kik <kikflip@gmail.com>
2015-08-16 20:41:31 +0200

    routing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,14 @@
 var ContactManager = new Marionette.Application();
 
+ContactManager.navigate = function(route, options) {
+	options || (options = {});
+	Backbone.history.navigate(route, options);
+};
+
+ContactManager.getCurrentRoute = function() {
+	return Backbone.history.fragment;
+};
+
 ContactManager.on("before:start", function(){
 	var RegionContainer = Marionette.LayoutView.extend({
         el: "#app-container",
@@ -10,6 +19,12 @@ ContactManager.on("before:start", function(){
     ContactManager.regions = new RegionContainer();
 });
 
-ContactManager.on("start", function() {    
-    ContactManager.ContactsApp.List.Controller.listContacts();
+ContactManager.on("start", function() {
+	if (Backbone.history) {
+		Backbone.history.start();
+
+		if(this.getCurrentRoute() === "") {
+			ContactManager.trigger("contacts:list");
+		}
+	}
 });
